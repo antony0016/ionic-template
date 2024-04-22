e373704664d944bc4360fb42097e07e11cef1507
sekixu <feather881030@gmail.com>
2024-04-23 03:35:16 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'motor-shop',
+  webDir: 'dist'
+};
+
+export default config;
